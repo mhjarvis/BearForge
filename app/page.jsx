4705c4051086ae32e70192d09e6360e01273d9ee
Markus Jarvis <markus.h.jarvis@gmail.com>
2025-04-14 10:55:08 -0500

    Refactor HomePage component to include Navbar and update global styles for consistent theming

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,15 @@
 // rafce - React Arrow Function Component Export
 
+import Navbar from "@/components/Navbar"
 import React from "react"
 
 const HomePage = () => {
-	return <div className="">HomePage</div>
+	return (
+		<div>
+			<div>Homepage</div>
+			<Navbar />
+		</div>
+	)
 }
 
 export default HomePage
---
+++ components/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Navbar = () => {
+	return <div className="bg-accent">Navbar</div>
+}
+
+export default Navbar
