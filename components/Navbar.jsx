ea0029be07c1e02cc01462f8e90d5962f845a99a
Markus Jarvis <markus.h.jarvis@gmail.com>
2025-04-15 16:04:11 -0500

    Implement Navbar component with navigation links for Home, About, and Services

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from "react"
 
 const Navbar = () => {
-	return <div className="bg-accent">Navbar</div>
+	return (
+		<nav class="fixed bottom-0 left-0 right-0 bg-white">
+			<div class="container mx-auto px-4">
+				<div class="flex justify-between items-center py-4">
+					<div class="flex space-x-4">
+						<a href="#" class="text-gray-800">
+							Home
+						</a>
+						<a href="#" class="text-gray-800">
+							About
+						</a>
+						<a href="#" class="text-gray-800">
+							Services
+						</a>
+					</div>
+				</div>
+			</div>
+		</nav>
+	)
 }
 
 export default Navbar
