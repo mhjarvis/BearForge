bf93c011fc7f6e1849836173c2be8063d0d2630e
Markus Jarvis <markus.h.jarvis@gmail.com>
2025-04-17 21:08:28 -0500

    Refactor Navbar component to position at the top and update structure for navigation links

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,22 +2,8 @@ import React from "react"
 
 const Navbar = () => {
 	return (
-		<nav class="fixed bottom-0 left-0 right-0 bg-white">
-			<div class="container mx-auto px-4">
-				<div class="flex justify-between items-center py-4">
-					<div class="flex space-x-4">
-						<a href="#" class="text-gray-800">
-							Home
-						</a>
-						<a href="#" class="text-gray-800">
-							About
-						</a>
-						<a href="#" class="text-gray-800">
-							Services
-						</a>
-					</div>
-				</div>
-			</div>
+		<nav className="fixed top-0 bg-white w-full h-10 flex flex-col items-center">
+			<div className="border ">f</div>
 		</nav>
 	)
 }
